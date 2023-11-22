da7782585277c91c7f6ac50d6557e4955f7eccc7
Danya <daniilerkesov122@gmail.com>
2023-11-22 14:40:40 +0300

    New style to-do list app

--- todo app/app.js
+++ todo app/app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// const input = document.querySelector("#input-box");
+// const addBtn = document.querySelector(".btn");
+// const listContainer = document.querySelector("#list-container");
+
+// function removeTask(task) {
+//     task.addEventListener("click", function () {
+//         this.classList.toggle("checked");
+//     });
+// }
+
+// addBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const inputValue = input.value;
+
+//     if (!inputValue) return;
+//     else {
+//         let taskList = document.createElement("li");
+//         taskList.classList.add("list-item");
+//         taskList.textContent = inputValue;
+//         listContainer.prepend(taskList);
+//         removeTask(taskList);
+//         input.value = "";
+//     }
+// });
