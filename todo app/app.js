45a87dd2e6520b013a8e437efe593164adbbcfa3
Danya <daniilerkesov122@gmail.com>
2023-11-24 13:17:08 +0300

    Add favicon icon

--- todo app/app.js
+++ todo app/app.js
@@ -3,6 +3,9 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector(".btn-input");
 const listContainer = document.querySelector(".list-container");
+const recycleBin = document.querySelector(".recycle-container");
+const recycleBtn = document.querySelector(".recycle-img");
+const recycledTasksList = document.querySelector(".recycled-tasks");
 
 function saveData() {
     const tasks = Array.from(listContainer.querySelectorAll(".task__item")).map(
@@ -13,12 +16,42 @@ function saveData() {
 
 function deleteTask(task) {
     task.addEventListener("click", function () {
+        const parentElement = this.parentElement; //task__item
         setTimeout(() => {
-            this.parentElement.remove();
+            recycledTasksList.insertAdjacentHTML(
+                "afterbegin",
+                `
+				<li class="task__item__container">
+					<span class="task__item">${this.textContent}</span>
+				</li>
+			`
+            );
+            parentElement.remove();
             saveData();
         }, 3000);
     });
 }
+
+function restoreTask(taskText) {
+    const taskItemContainer = document.createElement("li");
+    taskItemContainer.classList.add("task__item__container");
+
+    taskItemContainer.innerHTML = `
+        <span class="task__item">${taskText}</span>
+    `;
+
+    listContainer.insertAdjacentElement("afterbegin", taskItemContainer);
+}
+
+recycledTasksList.addEventListener("click", (e) => {
+    const taskItem = e.target.closest(".task__item");
+
+    if (taskItem) {
+        restoreTask(taskItem.textContent);
+        taskItem.parentElement.remove();
+        saveData();
+    }
+});
 
 listContainer.addEventListener("click", (e) => {
     const taskItem = e.target.closest(".task__item");
